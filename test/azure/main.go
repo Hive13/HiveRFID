2efee39d2ffa7e284551362853f415898de23721
Chris Hodapp <hodapp87@gmail.com>
2021-08-18 08:40:36 -0400

    Add more Azure scratch code

--- test/azure/main.go
+++ test/azure/main.go
@@ -6,10 +6,39 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"encoding/json"
 
 	"golang.org/x/oauth2/clientcredentials"
 )
+
+type User struct {
+	Ctxt   string       `json:"@odata.context"`
+	Value  []UserValue  `json:"value"`
+}
+
+type UserValue struct {
+	DisplayName			string    `json:"displayName"`
+	GivenName			string    `json:"givenName"`
+	Surname				string    `json:"surname"`
+	Mail				string    `json:"mail"`
+	UserPrincipalName	string    `json:"userPrincipalName"`
+	Id					string    `json:"id"`
+}
+
+func UsersGraphURL(suffix string, query map[string]string) (string, error) {
+	u, err := url.Parse("https://graph.microsoft.com/v1.0/users" + suffix)
+	if err != nil {
+		return "", err
+	}
+
+	q := u.Query()
+	for k,v := range query {
+		q.Set(k, v)
+	}
+	u.RawQuery = q.Encode()
+	return u.String(), nil
+}
 
 func main() {
 	//tenant := "50e3c41c-79b1-45e2-bc89-e119ffc7c692"
@@ -18,10 +47,7 @@ func main() {
 		"https://login.microsoftonline.com/%s/oauth2/v2.0/token",
 		tenant)
 
-	user_id := "178563"
-	rq_url := "https://graph.microsoft.com/v1.0/users?$filter=extension_243b36ba13e74e8e86cdbb23779f1e1a_hiveBadgeId%20eq%20" + user_id
-	//rq_url := "https://graph.microsoft.com/v1.0/users"
-
+	// Get OAuth token:
 	ctx := context.Background()
 	conf := clientcredentials.Config{
 		ClientSecret: client_secret,
@@ -39,21 +65,65 @@ func main() {
 
 	client := conf.Client(ctx)
 
-	//log.Printf("Using user ID: %s", user_id)
-	log.Printf("Using request URL: %s", rq_url)
-	
-	resp, err := client.Get(rq_url)
+	// Build request URL for given badge number:
+	badge_id := "178563"
+	rq_user_url, err := UsersGraphURL("", map[string]string {
+		"$filter": "extension_243b36ba13e74e8e86cdbb23779f1e1a_hiveBadgeId eq " + badge_id,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Using request URL: %s", rq_user_url)
+
+	// Query user from this:
+	resp, err := client.Get(rq_user_url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	if resp.StatusCode != http.StatusOK {
-		log.Printf("Response: %s", resp.Status)
-	} else {
-		log.Printf("resp=%+v", resp)
-		b, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("Body: %s", b)
+		log.Fatalf("Response: %s", resp.Status)
 	}
+	
+	log.Printf("resp=%+v", resp)
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Body: %s", b)
+
+	var user User
+	err = json.Unmarshal(b, &user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("User: %+v", user)
+
+	// Try to query group membership:
+	membership_url, err := UsersGraphURL(
+		"/" + user.Value[0].Id + "/transitiveMemberOf/microsoft.graph.group/",
+		map[string]string {
+			"$count": "true",
+			"$select": "id,displayName",
+			// TODO: Figure out why the below gives 400 Bad Request
+			// "$search": "\"displayName:Active Members\"",
+		})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Using request URL: %s", membership_url)
+	resp, err = client.Get(membership_url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Response: %s", resp.Status)
+	}
+
+	log.Printf("resp=%+v", resp)
+	b, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Body: %s", b)
 }
