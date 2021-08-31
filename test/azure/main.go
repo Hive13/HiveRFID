a1863488c41682d8552623a32e4bd03478a6b77d
rmanders <ryan.anderson.01@gmail.com>
2021-08-30 23:06:08 -0400

    calls the /checkMemberGroups endpoint with the GUID of the 'Active Members group to check membership

--- test/azure/main.go
+++ test/azure/main.go
@@ -1,29 +1,43 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"encoding/json"
 
 	"golang.org/x/oauth2/clientcredentials"
 )
 
+const (
+	active_members_guid = "8194af1c-7c5e-486d-a175-c5b3d4e08792"
+)
+
 type User struct {
-	Ctxt   string       `json:"@odata.context"`
-	Value  []UserValue  `json:"value"`
+	Ctxt  string      `json:"@odata.context"`
+	Value []UserValue `json:"value"`
+}
+
+type Groups struct {
+	Ctxt  string   `json:"@odata.context"`
+	Value []string `json:"value"`
 }
 
 type UserValue struct {
-	DisplayName			string    `json:"displayName"`
-	GivenName			string    `json:"givenName"`
-	Surname				string    `json:"surname"`
-	Mail				string    `json:"mail"`
-	UserPrincipalName	string    `json:"userPrincipalName"`
-	Id					string    `json:"id"`
+	DisplayName       string `json:"displayName"`
+	GivenName         string `json:"givenName"`
+	Surname           string `json:"surname"`
+	Mail              string `json:"mail"`
+	UserPrincipalName string `json:"userPrincipalName"`
+	Id                string `json:"id"`
+}
+
+type GroupQuery struct {
+	GroupIds []string `json:"groupIds"`
 }
 
 func UsersGraphURL(suffix string, query map[string]string) (string, error) {
@@ -33,7 +47,7 @@ func UsersGraphURL(suffix string, query map[string]string) (string, error) {
 	}
 
 	q := u.Query()
-	for k,v := range query {
+	for k, v := range query {
 		q.Set(k, v)
 	}
 	u.RawQuery = q.Encode()
@@ -51,7 +65,7 @@ func main() {
 	ctx := context.Background()
 	conf := clientcredentials.Config{
 		ClientSecret: client_secret,
-		ClientID: client_id,
+		ClientID:     client_id,
 		Scopes: []string{
 			"https://graph.microsoft.com/.default",
 		},
@@ -67,7 +81,7 @@ func main() {
 
 	// Build request URL for given badge number:
 	badge_id := "178563"
-	rq_user_url, err := UsersGraphURL("", map[string]string {
+	rq_user_url, err := UsersGraphURL("", map[string]string{
 		"$filter": "extension_243b36ba13e74e8e86cdbb23779f1e1a_hiveBadgeId eq " + badge_id,
 	})
 	if err != nil {
@@ -83,7 +97,7 @@ func main() {
 	if resp.StatusCode != http.StatusOK {
 		log.Fatalf("Response: %s", resp.Status)
 	}
-	
+
 	log.Printf("resp=%+v", resp)
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -99,20 +113,24 @@ func main() {
 	log.Printf("User: %+v", user)
 
 	// Try to query group membership:
-	membership_url, err := UsersGraphURL(
-		"/" + user.Value[0].Id + "/transitiveMemberOf/microsoft.graph.group/",
-		map[string]string {
-			"$count": "true",
-			"$select": "id,displayName",
-			// TODO: Figure out why the below gives 400 Bad Request
-			// "$search": "\"displayName:Active Members\"",
-		})
+
+	groupQuery := GroupQuery{
+		GroupIds: []string{active_members_guid},
+	}
+
+	var jsonGroupQuery []byte
+	jsonGroupQuery, err = json.Marshal(groupQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	membership_url, err := UsersGraphURL("/"+user.Value[0].Id+"/checkMemberGroups", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Printf("Using request URL: %s", membership_url)
-	resp, err = client.Get(membership_url)
+	resp, err = client.Post(membership_url, "application/json", bytes.NewBuffer(jsonGroupQuery))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -126,4 +144,17 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Printf("Body: %s", b)
+
+	var user_groups Groups
+	err = json.Unmarshal(b, &user_groups)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Returned array should have group UUID if user is a member. Empty mean not a member
+	if len(user_groups.Value) <= 0 {
+		log.Fatal("Not in [Active Members] group")
+	}
+
+	log.Printf("SUCCESS: User Groups: %+v", user_groups)
 }
